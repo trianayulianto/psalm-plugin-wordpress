b9ad545ed7c29b1c9587bd3a916770d150a06531
Chauncey McAskill <chauncey@mcaskill.ca>
2022-11-14 13:51:52 -0500

    Update stub overrides
    Resolves #38
    
    Updated overrides to reflect WordPress 6.1.
    
    Changed:
    - Removed `HOUR_IN_SECONDS` now provided by php-stubs/wordpress-globals.
    - Removed `wp_upload_dir()` and `wp_get_attachment_metadata()` as now correctly hinted in  php-stubs/wordpress-stubs.
    - Removed `add_filter()` and `add_action()` as extraneous.
    - Fixed missing parameter of `wp_parse_url()`.
    - Expanded array shapes over multiple lines to improve readability.

--- stubs/overrides.php
+++ stubs/overrides.php
@@ -1,142 +1,129 @@
 <?php
 
-define( 'HOUR_IN_SECONDS', 3600 );
-
 class WP_CLI {
 	/**
 	 *
-	 * @param string $command
-	 * @param callable|class-string $class
-	 * @param array{before_invoke?: callable, after_invoke?: callable, shortdesc?: string, longdesc?: string, synopsis?: string, when?: string, is_deferred?: bool} $args
+	 * @param string $name
+	 * @param callable|class-string $callable
+	 * @param array{
+	 *     before_invoke?: callable,
+	 *     after_invoke?: callable,
+	 *     shortdesc?: string,
+	 *     longdesc?: string,
+	 *     synopsis?: string,
+	 *     when?: string,
+	 *     is_deferred?: bool
+	 * } $args
 	 * @return bool
 	 */
-	static function add_command( string $command, $class, array $args = [] ) {
-
-	}
+	public static function add_command( string $name, $callable, array $args = [] ) {}
 
 	/**
-	 * @param WP_Error|string $message
+	 * @param string|WP_Error|Exception|Throwable $message
 	 * @param bool|int $exit
-	 *
 	 * @return (
 	 *     $exit is true
 	 *     ? no-return
 	 *     : null
 	 * )
 	 */
-	public static function error( $message, $exit = true ) {
-	}
+	public static function error( $message, $exit = true ) {}
 }
 
 class wpdb {
 	/**
 	 * @param string $query
 	 * @param array<scalar>|scalar $args
-	 * @return string|null
+	 * @return string|void
 	 */
-	public function prepare( $query, $args ) {
+	public function prepare( $query, $args ) {}
 
-	}
 	/**
-	 * Undocumented function
-	 *
-	 * @param string? $query
-	 * @param 'OBJECT'|'ARRAY_A'|'ARRAY_N'|'OBJECT_K' $object
+	 * @param string|null $query
+	 * @param 'ARRAY_A'|'ARRAY_N'|'OBJECT'|'OBJECT_K' $object
 	 * @return (
 	 *     $object is 'OBJECT'
 	 *     ? list<ArrayObject<string, string>>
 	 *     : ( $object is 'ARRAY_A'
 	 *         ? list<array<string, scalar>>
 	 *         : ( $object is 'ARRAY_N'
-	 *               ? list<array<scalar>>
-	 *               : array<string, ArrayObject<string, scalar>>
+	 *             ? list<array<scalar>>
+	 *             : array<string, ArrayObject<string, scalar>>
 	 *           )
-	 *    )
-	 * )
+	 *       )
+	 * )|null
 	 */
-	public function get_results( $query = null, $object = 'OBJECT' ) {
-
-	}
-}
-
-
-/**
- * @return array{path: string, basedir: string, baseurl: string, url: string}
- */
-function wp_upload_dir() {
-
+	public function get_results( $query = null, $object = 'OBJECT' ) {}
 }
 
 /**
- * @return array{path: string, basedir: string, baseurl: string, url: string}
+ * @return array{
+ *   path: string,
+ *   url: string,
+ *   subdir: string,
+ *   basedir: string,
+ *   baseurl: string,
+ *   error: string|false,
+ * }
  */
-function wp_get_upload_dir() {
-
-}
+function wp_get_upload_dir() {}
 
 /**
  * @template TFilterValue
- * @param string $a
+ * @param string $hook_name
  * @psalm-param TFilterValue $value
  * @return TFilterValue
  */
-function apply_filters( string $a, $value, ...$args ) {}
-
-function add_filter( string $filter, callable $function, int $priority = 10, int $args = 1 ) {}
-
-function add_action( string $filter, callable $function, int $priority = 10, int $args = 1 ) {}
+function apply_filters( string $hook_name, $value, ...$args ) {}
 
 /**
- * @param integer $attachment_id
- * @param boolean $skip_filters
- * @return array{width?: int, height?: int, sizes?: array<string,array{width: int, height: int, file: string}>, file: string}
- */
-function wp_get_attachment_metadata( int $attachment_id, $skip_filters = false ) {}
-
-/**
- *
  * @param string $url
- * @return array{path?: string, scheme?: string, host?: string, port?: int, user?: string, pass?: string, query?: string, fragment?: string}
+ * @param int $component
+ * @return (
+ *     $component is -1
+ *     ? array{
+ *           path?: string,
+ *           scheme?: string,
+ *           host?: string,
+ *           port?: int,
+ *           user?: string,
+ *           pass?: string,
+ *           query?: string,
+ *           fragment?: string
+ *       }
+ *     : string|int|false
+ *  )
  */
-function wp_parse_url( string $url ) {}
+function wp_parse_url( string $url, int $component = -1 ) {}
 
 /**
- *
  * @param string $option
  * @param mixed $default
  * @return mixed
  */
 function get_option( string $option, $default = null ) {}
 
-
 /**
- *
  * @param string $path
- * @param "https"|"http"|"relative"|"rest" $scheme
+ * @param 'https'|'http'|'relative'|'rest' $scheme
  * @return string
  */
-function home_url( string $path = null, $scheme = null ) : string {
-
-}
+function home_url( string $path = '', $scheme = null ) : string {}
 
 /**
- *
  * @template Args of array<array-key, mixed>
  * @template Defaults of array<array-key, mixed>
  * @psalm-param Args $args
  * @psalm-param Defaults $defaults
  * @psalm-return Defaults&Args
  */
-function wp_parse_args( $args, $defaults ) {
-}
+function wp_parse_args( $args, $defaults ) {}
 
 /**
  * @param WP_Error|mixed $error
  * @psalm-assert-if-true WP_Error $error
  */
-function is_wp_error( $error ) : bool {
-
-}
+function is_wp_error( $error ) : bool {}
 
 /**
  * @template T
@@ -145,6 +132,4 @@ function is_wp_error( $error ) : bool {
  * @param K $column
  * @return list<T>
  */
-function wp_list_pluck( array $list, string $column, string $index_key = null ) : array {
-
-}
+function wp_list_pluck( array $list, string $column, string $index_key = null ) : array {}
